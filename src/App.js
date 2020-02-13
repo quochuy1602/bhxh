4a130705d8631b55981dc41e0bd74e8f7f3fa02b
quochuy1602 <quochuy1602@gmail.com>
2020-02-13 03:20:39 -0600

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello world 2020</div>
+        <div>Hello world 2020 ! BHXH </div>
 
       </header>
     </div>
