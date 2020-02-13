08f4896a67e8cea7873ab6a48c6212d6dc245b00
quochuy1602 <quochuy1602@gmail.com>
2020-02-13 03:16:03 -0600

    Updates

---
+++ precache-manifest.12b4b4a0faf3eefc7e87d878753d3d79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092d82c74206696818b9a51fdb9138fc",
+    "url": "/bhxh/index.html"
+  },
+  {
+    "revision": "d9a964f76ad16cb96efd",
+    "url": "/bhxh/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4ad2ad25fafcdb855c7",
+    "url": "/bhxh/static/js/2.0b204000.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/bhxh/static/js/2.0b204000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a964f76ad16cb96efd",
+    "url": "/bhxh/static/js/main.2accf67c.chunk.js"
+  },
+  {
+    "revision": "f2d971651e0eb6ee9d6e",
+    "url": "/bhxh/static/js/runtime-main.1b49bf1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bhxh/static/media/logo.5d5d9eef.svg"
+  }
+]);
